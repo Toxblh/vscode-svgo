ef450d65c00d19f058cb3303791285b65c9b997e
Anton Palgunov <toxblh@gmail.com>
2017-08-29 01:01:20 +0300

    work context in editor and. And two work commands, mocks

--- extension.js
+++ extension.js
@@ -1,29 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "svgo" is now active!');
+    let pretty = vscode.commands.registerCommand('svgo.pretty', function () {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+        let text = editor.document.getText();
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        console.log('pretty', text)
+        vscode.window.showInformationMessage('pretty done');
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(pretty);
+
+    let minify = vscode.commands.registerCommand('svgo.minify', function () {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+        let text = editor.document.getText();
+
+        console.log('minify', text)
+        vscode.window.showInformationMessage('minify done');
+    });
+
+    context.subscriptions.push(minify);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
